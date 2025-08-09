baf16c2e87d10f391aca66cbd2c337ae294fe768
adhuhaam <adhuhamlayaal55@gmail.com>
2025-08-09 21:43:05 +0500

    26

--- index.php
+++ index.php
@@ -634,7 +634,7 @@ if ($_POST && isset($_POST['contact_form'])) {
                     About <span class="bg-gradient-to-r from-brand-blue to-brand-teal bg-clip-text text-transparent">Sky Border Solutions</span>
                 </h2>
                 <?php if (!empty($companyInfo['about_us'])): ?>
-                    <div class="mt-8 text-lg leading-8 text-gray-600 dark:text-gray-300 max-w-3xl mx-auto">
+                    <div class="mt-8 text-lg leading-8 text-gray-600 dark:text-gray-300 max-w-6xl mx-auto">
                         <?php echo nl2br(htmlspecialchars($companyInfo['about_us'])); ?>
                     </div>
                 <?php else: ?>
